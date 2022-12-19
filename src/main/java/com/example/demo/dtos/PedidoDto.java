31a0dbd1d717370d18b7445b182b7c4ae6d274da
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-12-19 10:25:05 -0300

    Adicionado anotações lombok

--- src/main/java/com/example/demo/dtos/PedidoDto.java
+++ src/main/java/com/example/demo/dtos/PedidoDto.java
@@ -2,9 +2,13 @@ package com.example.demo.dtos;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PedidoDto {
 
 	@NotBlank
